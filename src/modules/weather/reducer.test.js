7b82a0be52768c82adceee51f71018322f1455d9
Вадим Стрельцов <vadimstreltsov@MacBook-Air-Vadim-2.local>
2019-05-13 16:58:26 +0300

    minor changes

--- src/modules/weather/reducer.test.js
+++ src/modules/weather/reducer.test.js
@@ -1,8 +1,11 @@
 import { isLoading, weatherInfo } from "./reducer";
-import { GET_WEATHER_REQUEST } from "./actions";
 
-describe("Reducers", () => {
+const mockAction = {
+  type: "GET_WEATHER_REQUEST"
+};
+
+describe("Is loading reducer", () => {
   it("Should change state", () => {
-    expect(isLoading(false, GET_WEATHER_REQUEST)).toEqual(true);
+    expect(isLoading(false, mockAction)).toEqual(true);
   });
 });
